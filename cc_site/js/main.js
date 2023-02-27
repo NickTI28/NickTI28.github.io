a143400b99346aa3e1393217670dee74bc1c865d
Nicolas P <104401203+NickTI28@users.noreply.github.com>
2023-02-27 15:45:23 -0300

    Add files via upload

--- cc_site/js/main.js
+++ cc_site/js/main.js
@@ -12,3 +12,34 @@ for (i = 0; i < faq.length; i++) {
         }
     });
 }
+function readmore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("rd-btn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Leia mais"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Leia menos"; 
+      moreText.style.display = "inline";
+    }
+}
+
+function readMore2() {
+    var dots = document.getElementById("dots-2");
+    var moreText = document.getElementById("more-2");
+    var btnText = document.getElementById("rd-btn-2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Leia mais"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Leia menos"; 
+      moreText.style.display = "inline";
+    }
+}
