51c87577e6e72d6c32e9001c72912035f41f5675
Nicolas P <104401203+NickTI28@users.noreply.github.com>
2023-03-08 14:15:49 -0300

    Minimal changes

--- cc_site/js/main.js
+++ cc_site/js/main.js
@@ -1,16 +1,5 @@
-var faq = document.getElementsByClassName("faq-page");
-var i;
-
-for (i = 0; i < faq.length; i++) {
-    faq[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
-        } else {
-            body.style.display = "block";
-        }
-    });
+function smoothScrolling() {
+    document.documentElement.style.scrollBehavior = "smooth";
 }
 function readMore() {
     var dots = document.getElementById("dots");
